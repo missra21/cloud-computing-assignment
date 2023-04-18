6c6db99a030e2168b428d48e56f92b4521178c2b
Akash Mishra <akash.mishra01@sap.com>
2023-04-18 23:02:08 +0530

    sample SpringBoot Application

---
+++ src/main/java/com/sap/controller/SampleController.java
@@ -0,0 +1,13 @@
+package com.sap.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @GetMapping
+    public String sayHello(){
+        return "Hello from Spring!";
+    }
+}
